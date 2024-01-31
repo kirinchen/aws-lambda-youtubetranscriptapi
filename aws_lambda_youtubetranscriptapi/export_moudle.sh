eff57c4468ba16dfe70aae144e084d49d9f0fd15
kirin <kirin.chen1001@gmail.com>
2024-01-31 14:03:43 +0800

    init commit

---
+++ aws_lambda_youtubetranscriptapi/export_moudle.sh
@@ -0,0 +1,6 @@
+poetry export --without-hashes --format=requirements.txt > requirements.txt
+
+docker run -it --rm -v C:/Users/kirin/Desktop/AI/aws-lambda-youtubetranscriptapi/aws_lambda_youtubetranscriptapi/:/tmp python:3.11 bash -c "pip install -r tmp/requirements.txt -t tmp/dest/; exit"
+
+cp -r ./src/ ./dest/
+cp lambda_function.py ./dest/
